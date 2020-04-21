cea8bf42ee2f478bccc50c6cffb9adcbfaa8cebb
Dominik Ferber <dominik.ferber@gmail.com>
2020-04-21 13:09:53 +0200

    always go through worker
    This way we can use the Cloudflare worker analytics to track actual usage. In case usage gets too large, we can use 301 again.

--- workers-site/index.js
+++ workers-site/index.js
@@ -57,7 +57,7 @@ async function handleEvent(event) {
   let location = getCodeSandboxLocation(path);
 
   if (location) {
-    return Response.redirect(location, DEBUG ? 302 : 301);
+    return Response.redirect(location, 302);
   }
 
   try {
